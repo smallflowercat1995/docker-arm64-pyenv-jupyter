32899ca34bcbb723f0cc612307aabe00470a5803
刘栋19950828 <144557489+smallflowercat1995@users.noreply.github.com>
2023-10-15 02:22:44 -0500

    Update install.sh

--- build-pyenv/package/install.sh
+++ build-pyenv/package/install.sh
@@ -34,14 +34,12 @@ apt-get -y install apt-transport-https ca-certificates apt-utils eatmydata aptit
 eatmydata aptitude --without-recommends -o APT::Get::Fix-Missing=true -y update
 
 # 安装一些工具和 pyenv python 必备依赖
-eatmydata aptitude --without-recommends -o APT::Get::Fix-Missing=true -y install  gcc make aria2 \
-                                                                                  zlib1g-dev libbz2-dev libncurses-dev \
-                                                                                  libffi-dev libreadline-dev libssl-dev \
-                                                                                  liblzma-dev libgdbm-dev libgdbm-compat-dev \
-                                                                                  libsqlite3-dev uuid-dev tk-dev \
-                                                                                  build-essential libncurses5-dev libnss3-dev \
-                                                                                  git
-
+eatmydata aptitude --without-recommends -o APT::Get::Fix-Missing=true -y install  gcc make aria2 xz-utils \
+                                                                                  zlib1g-dev libbz2-dev libncurses-dev python-openssl \
+                                                                                  libffi-dev libreadline-dev libssl-dev llvm \
+                                                                                  liblzma-dev libgdbm-dev libgdbm-compat-dev git \
+                                                                                  libsqlite3-dev uuid-dev tk-dev libnss3-dev \
+                                                                                  build-essential libncurses5-dev libncursesw5-dev 
 }
 
 install_pyenv(){
